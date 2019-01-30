e5bfd3bb889f435c1393a9adcfc92f06b94244a1
Emilien Chapon <emilien.chapon@cern.ch>
2019-01-30 13:13:59 +0100

    SF plotting

--- macros/plotSFs.C
+++ macros/plotSFs.C
@@ -7,7 +7,8 @@
 Double_t tnp_weight_ppb_wrapper(Double_t *x, Double_t *par) {
    if (par[2]<0.5) return tnp_weight_trg_ppb(x[0], par[1]);
    else if (par[2]<1.5) return tnp_weight_muid_ppb(x[0],par[0],par[1]);
-   else return tnp_weight_iso_ppb(x[0],par[0],par[1]);
+   else if (par[2]<2.5) return tnp_weight_iso_ppb(x[0],par[0],par[1]);
+   else return tnp_weight_isotk_ppb(x[0],par[0],par[1]);
 }
 
 float ptmin(float etamax) {
@@ -31,11 +32,11 @@ void plotSFs() {
    etamin = new float[3]; etamin[0]=0.; etamin[1]=1.2; etamin[2]=2.1;
    etamax = new float[3]; etamax[0]=1.2; etamax[1]=2.1; etamax[2]=2.4;;
 
-   const char* tags[3] = {"trg_","muid_","iso_"};
-   const char* names[3] = {"Trigger","MuID","Iso"};
-   const double range[3] = {0.1,0.1,0.1};
+   const char* tags[4] = {"trg_","muid_","iso_","isotk_"};
+   const char* names[4] = {"Trigger","MuID","PF Iso","Tk Iso"};
+   const double range[4] = {0.1,0.1,0.1,0.1};
 
-   for (int j=0; j<3; j++) {
+   for (int j=0; j<4; j++) {
       // ppb
       int ietamax = (j==0) ? 0 : 2; // for trigger, only 1 plot (eta dependence)
       for (int ieta=0; ieta<=ietamax; ieta++) {
@@ -63,11 +64,11 @@ void plotSFs() {
          TF1 *fp = new TF1("fp",tnp_weight_ppb_wrapper,ptminval,ptmaxval,3);
          fp->SetParameters(eta,-1,j);
          fp->SetLineColor(kCyan);
-         fp->Draw("same");
+         if (j<3) fp->Draw("same");
          TF1 *fm = new TF1("fm",tnp_weight_ppb_wrapper,ptminval,ptmaxval,3);
          fm->SetParameters(eta,-2,j);
          fm->SetLineColor(kCyan);
-         fm->Draw("same");
+         if (j<3) fm->Draw("same");
          TF1 *fbinned = new TF1("fbinned",tnp_weight_ppb_wrapper,ptminval,ptmaxval,3);
          fbinned->SetParameters(eta,-10,j);
          fbinned->SetLineColor(kGreen+2);
@@ -77,13 +78,13 @@ void plotSFs() {
          fnom->SetLineColor(kRed);
          fnom->Draw("same");
 
-         if (j==0) tleg->SetHeader(Form("#splitline{pPb, %s}{#eta #in [-2.4,2.4], p_{T}>15 GeV/c}",names[j]));
-         else tleg->SetHeader(Form("#splitline{pPb, %s}{#eta #in [%.1f,%.1f], p_{T}>15 GeV/c}",names[j],etamin[ieta],etamax[ieta]));
+         if (j==0) tleg->SetHeader(Form("#splitline{pPb, %s}{#eta #in [-2.4,2.4], p_{T}>7 GeV/c}",names[j]));
+         else tleg->SetHeader(Form("#splitline{pPb, %s}{#eta #in [%.1f,%.1f], p_{T}>7 GeV/c}",names[j],etamin[ieta],etamax[ieta]));
 
          tleg->AddEntry(fnom,"Nominal","l");
          if (j>0) tleg->AddEntry("f1","stat (100 toys)","l");
          else tleg->AddEntry("f1","stat","l");
-         tleg->AddEntry(fp,"syst (+/1#sigma)","l");
+         if (j<3) tleg->AddEntry(fp,"syst (+/1#sigma)","l");
          if (j>0) tleg->AddEntry(fbinned,"binned","l");
          tleg->Draw();
          c1->SaveAs(Form("tnp_ppb_%seta%.1f-%.1f.pdf",tags[j],etamin[ieta],etamax[ieta]));
