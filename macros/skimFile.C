ce74f313e7480caf579e8fc0af2a98e126ba1c06
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2019-07-09 01:47:50 +0200

    Modified the skimFile.C macro

--- macros/skimFile.C
+++ macros/skimFile.C
@@ -3,29 +3,33 @@
 #include <iostream>
 #include <math.h>
 
-TTree* trimTree(TTree* told)
+TTree* skimTree(TTree* told, const std::string& type, const int& ps)
 {
-  std::vector<std::string> varVec = {"pt", "eta", "mass", "dzPV", "dxyPV", "tag_hiNtracks","isNotMuonSeeded", "TM", "SoftHINoDxyz", "HLT_PAL1DoubleMuOpen"}; //without flags
-  //std::vector<std::string> varVec = { "pt", "eta", "mass", "tag_hiNtracks","isNotMuonSeeded", "TM", "SoftID", "HLT_PAL1DoubleMuOpen" }; //with flags
-
+  //std::vector<std::string> varVec = {"pt", "eta", "mass", "dzPV", "dxyPV", "tag_hiNtracks","isNotMuonSeeded", "TM", "SoftHINoDxyz", "HLT_PAL1DoubleMuOpen"}; //without flags
+  std::vector<std::string> varVec = { "pt", "eta", "mass", "tag_hiNtracks","isNotMuonSeeded", "TM", "SoftID", "HLT_PAL1DoubleMuOpen" }; //with flags
+  
+  std::string sel = "";
+  if      (type=="Trg" ) { sel = ("SoftID==1 && (mass>=2.6 && mass<=3.6)"); }
+  else if (type=="MuID") { sel = ("TM==1 && (mass>=2.6 && mass<=3.6)"); }
+  else if (type=="Trk" ) { sel = ("isNotMuonSeeded==1 && (mass>=2.6 && mass<=3.6)"); }
 
   told->SetBranchStatus("*",0);
-  for (auto& v : varVec) {
-    if (told->GetBranch(v.c_str())) told->SetBranchStatus(v.c_str(), 1);
-  }
-  const auto& tnew = told->CloneTree(-1,"fast");
+  for (auto& v : varVec) { if (told->GetBranch(v.c_str())) told->SetBranchStatus(v.c_str(), 1); }
+  const Long64_t& entries = told->GetEntriesFast()/ps;
+
+  const auto tnew = (sel!="" ? told->CopyTree(sel.c_str(), 0, entries) : told->CloneTree((ps>1 ? Long64_t(entries/ps) : -1),"fast"));
 
   return tnew;
 }
 
-void skimFile(const char *filein, const char *fileout) {
+void skimFile(const char *filein, const char *fileout, const std::string& type="", const int& ps=1) {
    TFile fin(filein);
    TFile fout(fileout,"RECREATE");
 
    fout.cd();
-   const auto& tdir_muidtrg = fout.mkdir("tpTree");
-   tdir_muidtrg->cd();
-   const auto& tr_muidtrg = trimTree((TTree*)fin.Get("tpTree/fitter_tree"));
+   const auto& tdir = fout.mkdir("tpTree");
+   tdir->cd();
+   const auto& tree = skimTree((TTree*)fin.Get("tpTree/fitter_tree"), type, ps);
 
    fout.Write();
    fout.Close();
