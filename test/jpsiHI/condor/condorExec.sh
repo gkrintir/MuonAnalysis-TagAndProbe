58a5e788dc0b4f96b8f2db998be6496ca713465a
Santona <stuli@ucdavis.edu>
2019-07-09 02:28:03 +0200

    fixing big in condorExec

--- test/jpsiHI/condor/condorExec.sh
+++ test/jpsiHI/condor/condorExec.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#cd /afs/cern.ch/work/j/jjay/public/TagAndProbe_pPb/CMSSW_8_0_30/src
-#eval `scramv1 runtime -sh`
-cd ../
-cmsenv
+cd /afs/cern.ch/user/s/stuli/stuliWork/public/TnP_run2/Current/CMSSW_8_0_30/src
+eval `scramv1 runtime -sh`
+cd MuonAnalysis/TagAndProbe/test/jpsiHI
 cmsRun tp_Ana_Trig_${1}_pPb.py $2
